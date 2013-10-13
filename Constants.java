6cb9632e6d9e639f6712ae319d587e3eb12c689b
FreeTymeKiyan <FreeTymeSunKiyan@gmail.com>
2013-10-13 16:31:37 -0400

    Wiped out package in order to use javac

--- Constants.java
+++ Constants.java
@@ -1,5 +1,3 @@
-package com.freetymekiyan.catrancher;
-
 public interface Constants {
     
     /**All the possible inputs*/
--- StringArrayComparator.java
+++ StringArrayComparator.java
@@ -1,5 +1,3 @@
-package com.freetymekiyan.catrancher;
-
 import java.util.Comparator;
 
 /**
